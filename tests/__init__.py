5028ed356e3fe30b821b3c47f28bb446ec95e70b
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-06-29 22:39:23 -0300

    Regenerate lazy wrapper exports

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,105 +3,10 @@
 
 from __future__ import annotations
 
-from flext_core.lazy import (
-    build_lazy_import_map,
-    install_lazy_exports,
-    merge_lazy_imports,
-)
+from flext_core.lazy import install_lazy_exports
+from tests._exports import TESTS_FLEXT_TARGET_ORACLE_WMS_LAZY_IMPORTS
 
-_LAZY_IMPORTS = merge_lazy_imports(
-    (
-        ".examples",
-        ".integration",
-        ".unit",
-    ),
-    build_lazy_import_map(
-        {
-            ".base": (
-                "TestsFlextTargetOracleWmsServiceBase",
-                "s",
-            ),
-            ".conftest": ("conftest",),
-            ".constants": (
-                "TestsFlextTargetOracleWmsConstants",
-                "c",
-            ),
-            ".examples": ("examples",),
-            ".examples.test_examples": ("TestsFlextTargetOracleWmsExamples",),
-            ".integration": ("integration",),
-            ".integration.test_oracle": ("TestsFlextTargetOracleWmsOracle",),
-            ".models": (
-                "TestsFlextTargetOracleWmsModels",
-                "m",
-            ),
-            ".protocols": (
-                "TestsFlextTargetOracleWmsProtocols",
-                "p",
-            ),
-            ".settings": ("TestsFlextTargetOracleWmsSettings",),
-            ".typings": (
-                "TestsFlextTargetOracleWmsTypes",
-                "t",
-            ),
-            ".unit": ("unit",),
-            ".unit.test_benchmarks": ("TestsFlextTargetOracleWmsBenchmarks",),
-            ".unit.test_catalog": ("TestsFlextTargetOracleWmsCatalog",),
-            ".unit.test_features": ("TestsFlextTargetOracleWmsFeatures",),
-            ".unit.test_module_governance": (
-                "TestsFlextTargetOracleWmsModuleGovernance",
-            ),
-            ".unit.test_oracle_wms_cli": ("TestsFlextTargetOracleWmsOracleWmsCli",),
-            ".unit.test_oracle_wms_factory": (
-                "TestsFlextTargetOracleWmsOracleWmsFactory",
-            ),
-            ".unit.test_oracle_wms_init": ("TestsFlextTargetOracleWmsOracleWmsInit",),
-            ".unit.test_quality": ("TestsFlextTargetOracleWmsQuality",),
-            ".unit.test_sinks": ("TestsFlextTargetOracleWmsSinks",),
-            ".unit.test_stream": ("TestsFlextTargetOracleWmsStream",),
-            ".unit.test_structure": ("TestsFlextTargetOracleWmsStructure",),
-            ".unit.test_target": ("TestsFlextTargetOracleWmsTarget",),
-            ".unit.test_wms_patterns": ("TestsFlextTargetOracleWmsWmsPatterns",),
-            ".unit.test_workflow": ("TestsFlextTargetOracleWmsWorkflow",),
-            ".utilities": (
-                "TestsFlextTargetOracleWmsUtilities",
-                "u",
-            ),
-            "flext_tests": (
-                "d",
-                "e",
-                "h",
-                "r",
-                "td",
-                "tf",
-                "tk",
-                "tm",
-                "tv",
-                "x",
-            ),
-        },
-    ),
-    exclude_names=(
-        "cleanup_submodule_namespace",
-        "install_lazy_exports",
-        "lazy_getattr",
-        "logger",
-        "merge_lazy_imports",
-        "output",
-        "output_reporting",
-        "pytest_addoption",
-        "pytest_collect_file",
-        "pytest_collection_modifyitems",
-        "pytest_configure",
-        "pytest_runtest_setup",
-        "pytest_runtest_teardown",
-        "pytest_sessionfinish",
-        "pytest_sessionstart",
-        "pytest_terminal_summary",
-        "pytest_warning_recorded",
-    ),
-    module_name=__name__,
-)
-
+_LAZY_IMPORTS = TESTS_FLEXT_TARGET_ORACLE_WMS_LAZY_IMPORTS
 
 install_lazy_exports(
     __name__,
