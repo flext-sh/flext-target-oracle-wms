a2bb487a6dea3aa2adc3ae238427166d8b257b8f
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 00:21:52 -0300

    fixes

--- src/flext_target_oracle_wms/_utilities/__init__.py
+++ src/flext_target_oracle_wms/_utilities/__init__.py
@@ -9,13 +9,13 @@ from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
     from flext_target_oracle_wms._utilities.client import (
-        FlextTargetOracleWmsUtilitiesClient as FlextTargetOracleWmsUtilitiesClient,
+        FlextTargetOracleWmsUtilitiesClient,
     )
     from flext_target_oracle_wms._utilities.helpers import (
-        FlextTargetOracleWmsUtilitiesHelpers as FlextTargetOracleWmsUtilitiesHelpers,
+        FlextTargetOracleWmsUtilitiesHelpers,
     )
     from flext_target_oracle_wms._utilities.service_runtime import (
-        FlextTargetOracleWmsServiceRuntime as FlextTargetOracleWmsServiceRuntime,
+        FlextTargetOracleWmsServiceRuntime,
     )
 _LAZY_IMPORTS = build_lazy_import_map(
     {
