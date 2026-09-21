e36a80d7a5150bb04debd2cd689918dad2391809
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 13:45:22 -0300

    chore(gen): converge projections with the published generator (d1c55b54f, one name per line)

--- src/flext_target_oracle_wms/__init__.py
+++ src/flext_target_oracle_wms/__init__.py
@@ -20,10 +20,9 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
+    from flext_core import d, h, r, x
     from flext_meltano import FlextMeltanoConstants as meltano_c, s
     from flext_oracle_wms import e
-
-    from flext_core import d, h, r, x
 
     from .__version__ import FlextTargetOracleWmsVersion
     from ._config import FlextTargetOracleWmsConfig, config
